b47cc197a317a5bbeff2fbf01b450b060c1fad7c
3TP2 <kuba.mondzelewski@gmail.com>
2023-01-04 14:39:48 +0100

    ukończenie nieskomplikowanej wersji wisielca

--- wisielec/MainWindow.xaml.cs
+++ wisielec/MainWindow.xaml.cs
@@ -14,28 +14,32 @@ namespace wisielec
         {
             InitializeComponent();
         }
-        
+
+        public int zycia = 5;
         public char[] haslo = new char[32];
         public char[] wypis = new char[32];
         public int[] uzyte = new int[32];
         public int dlugoscHasa;
+        public int wygrana = 0;
         
         private void WprowadzSłowo_OnClick(object sender, RoutedEventArgs e)
         {
             gra.Visibility = Visibility.Visible;
             wprowadzenie.Visibility = Visibility.Hidden;
-            string str = wprowadzenieHasła.Text;
+            string str = wprowadzenieHasła.Text.ToLower();
             dlugoscHasa = str.Length;
             for (int i = 0; i < dlugoscHasa; i++) {
                 haslo[i] = str[i];
             }
 
-            var litera = '>';
+            var litera = ' ';
             sprawdzenieHasła(litera);
         }
 
         void sprawdzenieHasła(char litera)
         {
+            var licz = 0;
+            var licz2 = 0;
             for (var i = 0; i < dlugoscHasa; i++)
             {
                 if (uzyte[i] != 1)
@@ -44,6 +48,8 @@ namespace wisielec
                     {
                         wypis[i] = litera;
                         uzyte[i] = 1;
+                        licz++;
+                        wygrana++;
                     }
                     else
                     {
@@ -51,7 +57,36 @@ namespace wisielec
                     }
                 }
             }
+            
+            if (licz == licz2)
+            {
+                zycia -= 1;
+                literki.Text += litera;
+                literki.Text += " ";
+            }
+            else
+            {
+                licz2 = licz;
+            }
+            
+            zycie.Text = Convert.ToString(zycia);
 
+            if (wygrana == dlugoscHasa)
+            {
+                MessageBox.Show("Gratulujuę zgadłeś słowo.", "Zamknij");
+                wprowadzenie.Visibility = Visibility.Visible;
+                gra.Visibility = Visibility.Hidden;
+                zycia = 5;
+            }
+            
+            if (zycia == -1)
+            {
+                MessageBox.Show("Straciłeś wszystkie życia, przegrałeś..", "Zamknij");
+                wprowadzenie.Visibility = Visibility.Visible;
+                gra.Visibility = Visibility.Hidden;
+                zycia = 5;
+            }
+            
             hasło.Text = null;
             for (var i = 0; i < dlugoscHasa; i++)
             {
@@ -64,7 +99,7 @@ namespace wisielec
         {
             if (litera.Text != null)
             {
-                char znak = Convert.ToChar(litera.Text);
+                char znak = Convert.ToChar(litera.Text.ToLower());
                 sprawdzenieHasła(znak);
             }
         }
